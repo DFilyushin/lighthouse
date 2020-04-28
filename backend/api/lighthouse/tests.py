7c28d5cb6add70741167c4e4d1ef2f5253ae5e7d
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-04-28 13:57:39 +0700

    удалён неиспользуемый модуль

--- backend/api/lighthouse/tests.py
+++
@@ -1,5 +0,0 @@
-from django.test import TestCase, Client
-from rest_framework.test import APIRequestFactory
-from .tests.test_api_domain import *
-from .tests.test_api_prod import *
-from .tests.test_api_store import *
