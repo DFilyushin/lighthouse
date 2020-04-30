3ac9d8961b1e79d8e58bf8741db5eb53548898c9
D. Filyushin <dmitriy.filyushin@yandex.ru>
2020-04-30 17:58:49 +0700

    fix initital migrations add fixtures to initial data

--- backend/api/docker-entrypoint.sh
+++ backend/api/docker-entrypoint.sh
@@ -7,15 +7,18 @@ echo "Collect static files"
 python manage.py collectstatic --noinput
 
 echo "Apply web migrations"
-python manage.py makemigrations 
+python manage.py makemigrations lighthouse
 python manage.py migrate
 
 echo "Create super user"
 python manage.py createadmin
 
-
-# echo "Create default group"
-# python manage.py loaddata ./fixtures/groups.json
+echo "Initial store"
+python manage.py loaddata ./fixtures/ref_unit.json
+python manage.py loaddata ./fixtures/ref_cost.json
+python manage.py loaddata ./fixtures/ref_org.json
+python manage.py loaddata ./fixtures/ref_staff.json
+python manage.py loaddata ./fixtures/ref_type_material.json
 
 echo "Starting server"
-gunicorn -w 3 lighthouse.wsgi -b 0.0.0.0:8000
+gunicorn -w 3 api.wsgi -b 0.0.0.0:8000
