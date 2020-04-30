0046e2a94245944e0cc33f27447a334a8a02ab1b
D. Filyushin <dmitriy.filyushin@yandex.ru>
2020-04-30 17:12:30 +0700

    add create admin user

---
+++ backend/api/lighthouse/management/commands/createadmin.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from django.conf import settings
+import string
+import random
+
+
+class Command(BaseCommand):
+
+    help = 'Create super admin user with cli'
+    pass_size = 12
+    pass_chars = string.ascii_uppercase + string.digits + string.ascii_lowercase
+
+    def generate_pass(self):
+        return ''.join(random.choice(self.pass_chars) for _ in range(self.pass_size))
+
+    def handle(self, *args, **options):
+        if User.objects.count() == 0:
+            for user in settings.ADMINS:
+                username = user[0].replace(' ', '')
+                email = user[1]
+                password = self.generate_pass()
+                print('Creating account for %s with pass: %s' % (username, password))
+                admin = User.objects.create_superuser(email=email, username=username, password=password)
+                admin.is_active = True
+                admin.is_admin = True
+                admin.save()
+        else:
+            print('Admin accounts can only be initialized if no Accounts exist')
---
+++ backend/api/lighthouse/management/commands/generatesecret.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+from django.conf import settings
+import string
+import random
+
+
+class Command(BaseCommand):
+
+    help = 'Generate new secret key'
+    pass_size = 50
+    pass_chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+
+    def generate_pass(self):
+        return ''.join(random.choice(self.pass_chars) for _ in range(self.pass_size))
+
+    def handle(self, *args, **options):
+        if settings.SECRET_KEY == 'EMPTY':
+            settings.SECRET_KEY = self.generate_pass()
+            print('Secret generate ok')
+        else:
+            print('Secret key exists!')
