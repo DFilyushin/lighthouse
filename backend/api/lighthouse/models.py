fc06f73c95a303bbcc95cd4f6a59a842d7198cf2
Dmitriy Filyushin <dmitriy.filyushin@yandex.ru>
2020-05-01 15:28:26 +0700

    добавлен предупреждающий комментарий

--- backend/api/lighthouse/models.py
+++ backend/api/lighthouse/models.py
@@ -2,3 +2,8 @@ from .appmodels.manufacture import *
 from .appmodels.org import *
 from .appmodels.sales import *
 from .appmodels.db_signals import *
+
+# =================================
+# Файл не удалять
+# Импортируются триггеры
+# =================================
