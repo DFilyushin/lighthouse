026d11da43e8474193f8a7f44b05c658e7da9bb0
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-05-11 10:26:33 +0700

    добавлены ендпоинты для рецептуры и сырья

---
+++ frontend/src/services/endpoints/FormulaEndpoint.ts
@@ -0,0 +1,53 @@
+import BaseAPIEndpoint from './BaseEndpoint'
+
+class FormulaEndpoint {
+
+    /**
+     * Получить список рецептур
+     * @param search - строка поиска
+     * @param limit - ограничение вывода
+     * @param offset - сдвиг при ограничении
+     */
+    static getFormulaList(search?: string, limit?: number, offset?: number){
+        const baseUrl = `${BaseAPIEndpoint.getBaseURL()}/formula`;
+        const url = new URL(baseUrl);
+        if (search) url.searchParams.append('search', search);
+        if (limit) url.searchParams.append('limit', limit.toString());
+        if (offset) url.searchParams.append('offset', offset.toString());
+        return url.href
+    }
+
+    /**
+     * Получить рецептуру по коду
+     * @param id Код рецептуры
+     */
+    static getFormulaItem(id: number){
+        return `${BaseAPIEndpoint.getBaseURL()}/formula/${id}/`
+    }
+
+    /**
+     * Удалить существующую рецептуру
+     * @param id
+     */
+    static deleteFormula(id: number){
+        return this.getFormulaItem(id)
+    }
+
+    /**
+     * Добавить новую рецептуру
+     */
+    static newFormula(){
+        return `${BaseAPIEndpoint.getBaseURL()}/formula/`
+    }
+
+    /**
+     * Изменить существующую рецептуру
+     * @param id Код рецептуры
+     */
+    static updateFormula(id: number){
+        return this.getFormulaItem(id)
+    }
+
+}
+
+export default FormulaEndpoint;
---
+++ frontend/src/services/endpoints/RawEndpoint.ts
@@ -0,0 +1,32 @@
+import BaseAPIEndpoint from "./BaseEndpoint";
+
+
+class RawEndpoint{
+
+    static getRawList(search?: string, limit?: number, offset?: number){
+        const baseUrl = `${BaseAPIEndpoint.getBaseURL()}/raw`;
+        const url = new URL(baseUrl);
+        if (search) url.searchParams.append('search', search);
+        if (limit) url.searchParams.append('limit', limit.toString());
+        if (offset) url.searchParams.append('offset', offset.toString());
+        return url.href
+    }
+
+    static getRawItem(id: number){
+        return `${BaseAPIEndpoint.getBaseURL()}/raw/${id}`
+    }
+
+    static deleteRaw(id: number){
+        return `${BaseAPIEndpoint.getBaseURL()}/raw/${id}/`
+    }
+
+    static newRaw(){
+        return `${BaseAPIEndpoint.getBaseURL()}/raw/`
+    }
+
+    static saveRaw(id: number){
+        return `${BaseAPIEndpoint.getBaseURL()}/raw/${id}/`
+    }
+}
+
+export default RawEndpoint;
