e55d61b4c14e9ec888c1f3685f415d1ee192176d
Dmitriy Filyushin <dmitriy.filyushin@yandex.ru>
2020-05-13 16:32:58 +0700

    удалён неисп. код

--- frontend/src/views/About/About.tsx
+++ frontend/src/views/About/About.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid, Typography } from '@material-ui/core';
-import {Link} from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
     root: {
