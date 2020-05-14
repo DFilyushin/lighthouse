70bcf684e4e5feb48b5b6f893756289e1abb6489
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-05-14 11:13:31 +0700

    удалён неисп. код

--- frontend/src/services/endpoints/EndpointInterface.ts
+++
@@ -1,8 +0,0 @@
-interface IEndpointInterface {
-    getItemList(search?: string, limit?: number, offset?: number): string;
-    getItem(id: number): string;
-    deleteItem(id: number): string;
-    updateItem(id: number): string;
-    newItem(): string;
-}
-export default IEndpointInterface;
