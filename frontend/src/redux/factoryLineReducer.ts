ebaa81c000eb83ef3b7044f74b61c04df7e10cfb
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-05-22 13:44:25 +0700

    добавлен редбюсер для произ. линий

---
+++ frontend/src/redux/factoryLineReducer.ts
@@ -0,0 +1,42 @@
+import {IFactoryLineState} from 'types/state/factorylines';
+import {
+    FACTORY_LINE_LOAD_START,
+    FACTORY_LINE_LOAD_FINISH,
+    FACTORY_LINE_LOAD_SUCCESS,
+    FACTORY_CLEAR_ERROR,
+    FACTORY_LINE_LOAD_ITEM,
+    FACTORY_LINE_DELETE_OK,
+    FACTORY_LINE_SET_ERROR, FACTORY_LINE_UPDATE
+} from 'redux/actions/types'
+
+const getInitState = () => ({
+    items: [],
+    lineItem: {id: 0, name: ''},
+    isLoading: false,
+    error: '',
+    typeMessage: '',
+    hasError: false
+});
+
+
+export const factoryLineReducer =  (state: IFactoryLineState = getInitState(), action: any) => {
+    switch (action.type) {
+        case FACTORY_LINE_LOAD_START:
+            return {...state, isLoading: true};
+        case FACTORY_LINE_LOAD_FINISH:
+            return {...state, isLoading: false};
+        case FACTORY_LINE_SET_ERROR:
+            return {...state, hasError: true, error: action.error};
+        case FACTORY_LINE_LOAD_SUCCESS:
+            return {...state, items: action.items};
+        case FACTORY_LINE_LOAD_ITEM:
+            return {...state, lineItem: action.item};
+        case FACTORY_CLEAR_ERROR:
+            return {...state, hasError: false, error: ''};
+        case FACTORY_LINE_UPDATE:
+            return {...state, lineItem: action.item};
+        case FACTORY_LINE_DELETE_OK:
+            return {...state, items: action.items};
+        default: return state;
+    }
+};
