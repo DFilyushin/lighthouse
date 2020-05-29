29853e6cc8f5955815ec2aa8501aec3247a99f41
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-05-29 18:05:05 +0700

    добавлена возможность добавления иконок в тулбар списков данных

--- frontend/src/components/DefaultToolbar/DefaultToolbar.tsx
+++ frontend/src/components/DefaultToolbar/DefaultToolbar.tsx
@@ -4,6 +4,10 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Button, Typography } from '@material-ui/core';
 import { SearchInput } from 'components';
 import { useHistory } from "react-router-dom";
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 
 const useStyles = makeStyles(theme => ({
     root: {},
@@ -30,7 +34,20 @@ const useStyles = makeStyles(theme => ({
     },
     searchInput: {
         marginRight: theme.spacing(1)
+    },
+    small: {
+        width: theme.spacing(3),
+        height: theme.spacing(3),
+    },
+    large: {
+        width: theme.spacing(7),
+        height: theme.spacing(7),
+    },
+    white: {
+        color: theme.palette.grey["100"],
+        backgroundColor: theme.palette.background.default
     }
+
 }));
 
 interface IDefaultToolbar {
@@ -41,7 +58,8 @@ interface IDefaultToolbar {
     findCaption: string,
     showDelete: boolean,
     onFind: any,
-    onDelete: any
+    onDelete: any,
+    icon?: any
 }
 
 const DefaultToolbar = (props: IDefaultToolbar) => {
@@ -66,7 +84,15 @@ const DefaultToolbar = (props: IDefaultToolbar) => {
             {...rest}
             className={clsx(classes.root, className)}
         >
-            <Typography variant="h5">{title}</Typography>
+            <List className={classes.root}>
+            <ListItem>
+                <ListItemAvatar>
+                    <Avatar className={clsx(classes.large, classes.white)}> {rest.icon} </Avatar>
+                </ListItemAvatar>
+                <Typography variant="h4">{title}</Typography>
+            </ListItem>
+        </List>
+
             <div className={classes.buttonGroup}>
                 <span className={classes.spacer} />
                 <Button color="primary" variant="contained" onClick={onNewItemButtonHandler}>{newItemTitle}</Button>
