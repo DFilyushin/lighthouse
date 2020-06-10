394c6920c75c716f88022476317e7b9f5961cd11
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-06-10 14:58:19 +0700

    компонент подключен в общий список

--- frontend/src/components/index.tsx
+++ frontend/src/components/index.tsx
@@ -2,3 +2,4 @@ export { default as RouteWithLayout } from './RouteWithLayout';
 export { default as SearchInput } from './SearchInput';
 export { default as Label} from './Label';
 export { default as DefaultToolbar } from './DefaultToolbar';
+export { default as NumberFormatCustom } from './NumberFormatCustom';
