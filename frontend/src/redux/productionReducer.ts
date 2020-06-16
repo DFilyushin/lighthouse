fc25fd6938fca7412c8590d9a456e946f66c46ff
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-06-16 10:34:20 +0700

    при окрытии карты очистка всех наборов данных

--- frontend/src/redux/productionReducer.ts
+++ frontend/src/redux/productionReducer.ts
@@ -38,7 +38,8 @@ export const productionReducer = (state = initState(), action: any) => {
         case PROD_LOAD_SUCCESS:
             return {...state, prodCardList: action.items};
         case PROD_LOAD_ITEM_SUCCESS:
-            return {...state, prodCardItem: action.item, canRedirect: false};
+            return {...state, prodCardItem: action.item, canRedirect: false,
+                prodCardCalc: [], prodCardList: [], prodCardTare: [], prodCardTeam: []};
         case PROD_CHANGE_ITEM:
             return {...state, prodCardItem: action.item};
         case PROD_TEAM_LOAD_SUCCESS:
