2b1d47418e0eefea0f288fada58d043184311dcd
Dmitriy Filyushin <dmitriy.filyushin@yandex.ru>
2020-06-21 22:47:25 +0700

    удалён неисп. код

--- frontend/src/components/Markdown/Markdown.tsx
+++ frontend/src/components/Markdown/Markdown.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
 import clsx from 'clsx';
 import Prism from 'prismjs';
 import ReactMarkdown from 'react-markdown/with-html';
