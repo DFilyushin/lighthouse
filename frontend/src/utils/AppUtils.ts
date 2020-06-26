382089b673aa239c16cf92905ff6b601d3ed1227
Dmitriy Filyushin <dmitriy.filyushin@yandex.ru>
2020-06-26 15:24:20 +0700

    добавлена функция проверки валидности почтового адреса

--- frontend/src/utils/AppUtils.ts
+++ frontend/src/utils/AppUtils.ts
@@ -23,3 +23,8 @@ export function  parseJwt (token: string) {
 export function getRandomInt(max: number) {
     return Math.floor(Math.random() * Math.floor(max));
 }
+
+export function validateEmail(email: string) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
