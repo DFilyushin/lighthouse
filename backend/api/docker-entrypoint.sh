9ac7b2b1cc003a6c465c57e8d3dd171024a514de
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-07-01 13:59:42 +0700

    some fix for test data import

--- backend/api/docker-entrypoint.sh
+++ backend/api/docker-entrypoint.sh
@@ -14,10 +14,12 @@ echo "Create super user"
 python manage.py createadmin
 
 echo "Initial store"
-python manage.py loaddata ./fixtures/user.json
-python manage.py loaddata ./fixtures/group.json
-python manage.py loaddata ./fixtures/user_groups.json
 
+echo "initial auth"
+python manage.py loaddata ./fixtures/group.json
+python manage.py loaddata ./fixtures/user.json
+
+echo "initial material, formula, compoenents"
 python manage.py loaddata ./fixtures/refmaterialtype.json
 python manage.py loaddata ./fixtures/materialunit.json
 python manage.py loaddata ./fixtures/tare.json
@@ -25,20 +27,24 @@ python manage.py loaddata ./fixtures/material.json
 python manage.py loaddata ./fixtures/formula.json
 python manage.py loaddata ./fixtures/formulacomp.json
 
+echo "initial org"
 python manage.py loaddata ./fixtures/org.json
 python manage.py loaddata ./fixtures/department.json
 python manage.py loaddata ./fixtures/staff.json
 python manage.py loaddata ./fixtures/employee.json
 
+echo "initial expense"
 python manage.py loaddata ./fixtures/refcost.json
 python manage.py loaddata ./fixtures/cost.json
 
+echo "initial factory"
 python manage.py loaddata ./fixtures/productionline.json
 python manage.py loaddata ./fixtures/manufacture.json
 python manage.py loaddata ./fixtures/prodteam.json
 python manage.py loaddata ./fixtures/prodcalc.json
 python manage.py loaddata ./fixtures/prodreadyproduct.json
 
+echo "initial sales"
 python manage.py loaddata ./fixtures/client.json
 python manage.py loaddata ./fixtures/contract.json
 python manage.py loaddata ./fixtures/contractspec.json
