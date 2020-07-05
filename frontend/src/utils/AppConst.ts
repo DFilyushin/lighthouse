acd72a240e5fb352fcbd8333c4e822d862b51a87
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-07-05 18:59:00 +0700

    добавлены константы групп пользователя

--- frontend/src/utils/AppConst.ts
+++ frontend/src/utils/AppConst.ts
@@ -1,1 +1,10 @@
 export const NEW_RECORD_VALUE = -1
+
+export const GROUP_ALL = 'all'
+export const GROUP_ADMIN = 'admin'
+export const GROUP_FACTORY = 'factory'
+export const GROUP_MANAGER = 'manager'
+export const GROUP_TECHNOLOGIST = 'technologist'
+export const GROUP_REPORT = 'report'
+export const GROUP_BOSS = 'boss'
+export const GROUP_FINANCE = 'finance'
