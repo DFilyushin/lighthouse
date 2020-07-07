37afc39ab8172f6b7dfa17a31b6da04d79db4a94
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-07-07 09:39:21 +0700

    добавлены константы интерфейса диалогов

--- frontend/src/utils/AppConst.ts
+++ frontend/src/utils/AppConst.ts
@@ -8,3 +8,12 @@ export const GROUP_TECHNOLOGIST = 'technologist'
 export const GROUP_REPORT = 'report'
 export const GROUP_BOSS = 'boss'
 export const GROUP_FINANCE = 'finance'
+
+export const DIALOG_TYPE_CONFIRM = 'Подтверждение'
+export const DIALOG_SELECT_TEXT = 'Выбрать'
+export const DIALOG_CANCEL_TEXT = 'Отменить'
+export const DIALOG_YES = 'Да'
+export const DIALOG_NO = 'Нет'
+
+
+export const DIALOG_ASK_DELETE = 'Удалить выбранные записи?'
