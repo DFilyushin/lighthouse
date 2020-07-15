0073f1053ecb9c7241e083a7f9ae7aa23e7fbf3e
Dmitriy Filyushin <dmitriy.filyushin@yandex.ru>
2020-07-15 23:03:45 +0700

    refactoring, clean code

--- backend/api/lighthouse/management/commands/generatesecret.py
+++ backend/api/lighthouse/management/commands/generatesecret.py
@@ -1,7 +1,5 @@
-from django.core.management.base import BaseCommand, CommandError
-from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
 from django.conf import settings
-import string
 import random
 
 
