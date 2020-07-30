f32e2788910a44a08eadc3376df9d2a08332c196
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-07-30 18:52:40 +0700

    добавлены настройки сохранения выбранных дат для журнала оплат

--- frontend/src/types/Settings.ts
+++ frontend/src/types/Settings.ts
@@ -4,3 +4,7 @@ export const PROD_PRODUCT = 'prodProduct';
 
 export const STORE_PERIOD_START = 'storeStart'
 export const STORE_PERIOD_END = 'storeEnd'
+
+export const PAYMENT_PERIOD_START = 'paymentStart'
+export const PAYMENT_PERIOD_END = 'paymentEnd'
+export const PAYMENT_METHOD = 'paymentMethod'
