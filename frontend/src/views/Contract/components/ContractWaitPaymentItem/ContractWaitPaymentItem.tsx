2f67531ab9d0b9b7478968d7414388e60b848266
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-08-11 19:42:10 +0700

    добавлены компоненты редактирования графика платежей

---
+++ frontend/src/views/Contract/components/ContractWaitPaymentItem/ContractWaitPaymentItem.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {IWaitPaymentContractItem} from "types/model/contract";
+import {
+    IconButton,
+    Fab,
+    Hidden,
+    Tooltip,
+    Paper,
+    TableCell,
+    TableRow,
+    TextField
+} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
+
+interface IContractWaitItemProps {
+    className: string;
+    match: any;
+    item: IWaitPaymentContractItem;
+    onDeleteItem: any;
+    onChangeItem: any;
+}
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: theme.spacing(1)
+    },
+    paper: {
+        width: '80%',
+        maxHeight: 435,
+    },
+    paper_root: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    paper_bar: {
+        flexGrow: 1,
+        padding: 1
+    },
+    formControl: {
+        margin: theme.spacing(0),
+    },
+    iconButton: {
+        padding: 10,
+    },
+}));
+
+
+const ContractWaitPaymentItem = (props: IContractWaitItemProps) => {
+    const { item, onDeleteItem, onChangeItem } = props;
+
+    const classes = useStyles();
+
+    return (
+        <TableRow>
+            <TableCell>
+                <TextField
+                    fullWidth
+                    margin="dense"
+                    name="waitDate"
+                    onChange={onChangeItem}
+                    required
+                    value={item.waitDate}
+                    id="waitDate"
+                    type="date"
+                    InputProps={{
+                        readOnly: true,
+                    }}
+                />
+            </TableCell>
+            <TableCell>
+                <TextField
+                    fullWidth
+                    type={'number'}
+                    margin="dense"
+                    name="waitSum"
+                    onChange={onChangeItem}
+                    required
+                    value={item.waitSum}
+                />
+            </TableCell>
+            <TableCell>
+                <Tooltip title={'Удалить запись'}>
+                    <Fab color="secondary" aria-label="add" onClick={event => onDeleteItem(item.id)}>
+                        <DeleteIcon />
+                    </Fab>
+                </Tooltip>
+            </TableCell>
+        </TableRow>
+    )
+}
+
+export default ContractWaitPaymentItem;
---
+++ frontend/src/views/Contract/components/ContractWaitPaymentTable/ContractWaitPaymentTable.tsx
@@ -0,0 +1,148 @@
+import React from "react"
+import moment from "moment"
+import {makeStyles} from "@material-ui/core/styles"
+import {useHistory} from "react-router-dom"
+import {
+    Button,
+    Card, CardActions,
+    CardContent,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableRow,
+    Typography
+} from "@material-ui/core"
+import clsx from "clsx"
+import PerfectScrollbar from "react-perfect-scrollbar"
+import {IWaitPaymentContractItem} from "types/model/contract"
+import {useDispatch} from "react-redux";
+import {
+    addNewWaitPaymentItem,
+    deleteContractSpecItem,
+    deleteWaitPaymentItem
+} from "../../../../redux/actions/contractAction";
+import ContractWaitPaymentItem from "../ContractWaitPaymentItem";
+
+
+export interface IContractWaitPaymentTableProps {
+    className: string;
+    contract: number;
+    items: IWaitPaymentContractItem[];
+    onClickTableItem: ( (id: number)=> void);
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {},
+    content: {
+        padding: 0
+    },
+    inner: {
+        minWidth: 1050
+    },
+    nameContainer: {
+        display: 'flex',
+        alignItems: 'center'
+    },
+    avatar: {
+        marginRight: theme.spacing(2)
+    },
+    actions: {
+        justifyContent: 'flex-end'
+    },
+    tableRow: {
+
+    },
+    addButton: {
+        marginRight: theme.spacing(2),
+        marginLeft: theme.spacing(2),
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2)
+    },
+}))
+
+
+
+const ContractWaitPaymentTable = (props: IContractWaitPaymentTableProps) => {
+    const { className, items, contract, onClickTableItem, ...rest } = props
+    const classes = useStyles()
+    const history = useHistory()
+    const dispatch = useDispatch();
+
+    /**
+     * Подсчитать общую сумму
+     * @param items
+     */
+    function getSubTotalPayment(items: IWaitPaymentContractItem[]) {
+        return items.map(({ waitSum }) => waitSum).reduce((sum, i) => sum + i, 0);
+    }
+
+    function onDeleteItem(id: number) {
+        console.log('!!!')
+        dispatch(deleteWaitPaymentItem(id))
+    }
+    
+    function onChangeItem(id: number) {
+
+    }
+
+    /**
+     * Добавление нового сырья в рецептуру
+     */
+    const handleAddNewPayment = () => {
+        dispatch(addNewWaitPaymentItem())
+    }
+
+    return (
+        <Card
+            {...rest}
+            className={clsx(classes.root, className)}
+        >
+            <CardContent className={classes.content}>
+                <PerfectScrollbar>
+                    <div className={classes.inner}>
+                        <Table>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Дата</TableCell>
+                                    <TableCell>Сумма</TableCell>
+                                    <TableCell/>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                <Button
+                                    className={classes.addButton}
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={(event) => {handleAddNewPayment()}}>
+                                    Добавить график
+                                </Button>
+                                {
+                                    items.map(item => (
+                                        <ContractWaitPaymentItem 
+                                            className={''} 
+                                            match={''} 
+                                            item={item} 
+                                            onDeleteItem={onDeleteItem}
+                                            onChangeItem={onChangeItem}
+                                        />
+                                    ))
+                                }
+                                <TableRow>
+                                    <TableCell>
+                                        <Typography variant="body1">Итого</Typography>
+                                    </TableCell>
+                                    <TableCell align={'right'}>{getSubTotalPayment(items).toLocaleString('ru-RU')}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </div>
+                </PerfectScrollbar>
+            </CardContent>
+            <CardActions>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default ContractWaitPaymentTable
