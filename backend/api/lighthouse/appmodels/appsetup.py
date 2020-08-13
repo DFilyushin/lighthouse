3ebc61f0c052043eca05d337923fa284047aea30
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-08-13 12:34:33 +0700

    добавлена модель Настроек приложения

---
+++ backend/api/lighthouse/appmodels/appsetup.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class AppSetup(models.Model):
+    code = models.CharField(max_length=20, null=False, blank=False, unique=True, verbose_name='Код настройки')
+    name = models.CharField(max_length=255, default='', null=False, blank=True, verbose_name='Описание')
+    int_value = models.IntegerField(default=0, verbose_name='Числовое значение')
+    flo_value = models.FloatField(default=0, verbose_name='Точное значение')
+    str_value = models.CharField(max_length=255, null=True, blank=True, verbose_name='Строковое значение')
+    date_value = models.DateTimeField(null=True, verbose_name='Дата/время')
+
+    class Meta:
+        verbose_name = 'Настройка приложения'
+        verbose_name_plural = 'Настройки приложения'
+        ordering = ['name']
