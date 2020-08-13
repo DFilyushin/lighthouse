754238d625d10a082ca603276a7e04e82cbe57f2
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-08-13 18:41:48 +0700

    добавлен строковый атрибут Тип настройки

--- backend/api/lighthouse/appmodels/appsetup.py
+++ backend/api/lighthouse/appmodels/appsetup.py
@@ -4,6 +4,7 @@ from django.db import models
 class AppSetup(models.Model):
     code = models.CharField(max_length=20, null=False, blank=False, unique=True, verbose_name='Код настройки')
     name = models.CharField(max_length=255, default='', null=False, blank=True, verbose_name='Описание')
+    kind = models.CharField(max_length=4, null=True, blank=True, verbose_name='Тип настройки')
     int_value = models.IntegerField(default=0, verbose_name='Числовое значение')
     flo_value = models.FloatField(default=0, verbose_name='Точное значение')
     str_value = models.CharField(max_length=255, null=True, blank=True, verbose_name='Строковое значение')
