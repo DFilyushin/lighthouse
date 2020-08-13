624886b9e15fcf1102f568816f9b81419b5fa371
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-08-13 12:34:49 +0700

    подключен новый файл с моделями базы данных

--- backend/api/lighthouse/models.py
+++ backend/api/lighthouse/models.py
@@ -2,6 +2,7 @@ from .appmodels.manufacture import *
 from .appmodels.org import *
 from .appmodels.sales import *
 from .appmodels.db_signals import *
+from .appmodels.appsetup import *
 
 # =================================
 # Файл не удалять
