13c82dba5de3ea7e614934d38b74134d06acb52d
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-08-13 22:36:53 +0700

    добавлено отображение настройки ставки НДС

--- frontend/src/views/Setup/Setup.tsx
+++ frontend/src/views/Setup/Setup.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Typography, Divider, colors } from '@material-ui/core';
-import {Page} from "components";
+import React, {useEffect} from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import {
+    Typography,
+    Divider,
+    colors,
+    Grid,
+    TextField, Card, CardContent, CardActions, Button
+} from '@material-ui/core'
+import {Page} from "components"
+import {useDispatch, useSelector} from "react-redux"
+import {IStateInterface} from "../../redux/rootReducer"
+import {changeNdsValue, getSetupNdsRate, updateSetupNdsRate} from "../../redux/actions/setupAction"
+import clsx from "clsx";
+import {useHistory} from "react-router-dom";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -27,8 +38,24 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
-const About = () => {
-    const classes = useStyles();
+const Setup = () => {
+    const classes = useStyles()
+
+    const dispatch = useDispatch()
+    const history = useHistory()
+    const ndsValue  = useSelector((state: IStateInterface)=> state.setup.nds)
+
+    useEffect(() => {
+        dispatch(getSetupNdsRate())
+    }, [dispatch])
+
+    function onChangeNdsHandler(event: React.ChangeEvent<HTMLInputElement>) {
+        dispatch(changeNdsValue(parseFloat(event.target.value)))
+    }
+
+    function saveHandler(event: React.SyntheticEvent) {
+        dispatch(updateSetupNdsRate())
+    }
 
     return (
         <Page
@@ -45,10 +72,53 @@ const About = () => {
             <Divider className={classes.divider} />
                 <div className={classes.markdownContainer}>
                 </div>
+            <Card
+                className={clsx(classes.root)}
 
+            >
+                <CardContent>
+                    <Grid
+                        container
+                        spacing={3}
+                    >
+                        <Grid
+                            item
+                            xs={2}
+                        >
+                            <TextField
+                                fullWidth
+                                label="Ставка НДС"
+                                margin="dense"
+                                name="name"
+                                onChange={onChangeNdsHandler}
+                                required
+                                value={ndsValue}
+                                variant="outlined"
+                            />
+                        </Grid>
+                    </Grid>
+                </CardContent>
+                <Divider />
+                <CardActions>
+                    <Button
+                        color="primary"
+                        variant="contained"
+                        onClick={saveHandler}
+                    >
+                        Сохранить
+                    </Button>
+                    <Button
+                        color="secondary"
+                        variant="contained"
+                        onClick={(event => history.push('/'))}
+                    >
+                        Отменить
+                    </Button>
+                </CardActions>
+            </Card>
         </Page>
     );
 };
 
-export default About;
+export default Setup;
 
