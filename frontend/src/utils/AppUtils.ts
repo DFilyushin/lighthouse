4d3fa18b966e3e24997cdadf4c49586c249232bd
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-08-17 12:48:53 +0700

    добавлена функция округления до сотых

--- frontend/src/utils/AppUtils.ts
+++ frontend/src/utils/AppUtils.ts
@@ -29,3 +29,8 @@ export function validateEmail(email: string) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
+
+
+export function RoundValue(value: number) {
+    return Math.round((value + Number.EPSILON) * 100) / 100
+}
