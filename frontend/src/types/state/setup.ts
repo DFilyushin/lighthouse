cf06e9a613707526f703d45c064ded4477234843
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-08-26 23:39:41 +0700

    расширено состояние настроек приложения, добавлена опция "Количество дней по умолчанию для установки резерва продукции"

--- frontend/src/types/state/setup.ts
+++ frontend/src/types/state/setup.ts
@@ -1,5 +1,15 @@
+export const SETUP_NDS = 'NDS'
+export const SETUP_RESERVE_INTERVAL = 'RESERVE_PERIOD'
+
+
 export interface ISetupState {
+    /**
+     * значение НДС
+     */
     nds: number;
-    mail: string;
+    /**
+     * Количество дней по умолчанию для установки резерва продукции
+     */
+    reserveInterval: number;
     loaded: string | null;
 }
