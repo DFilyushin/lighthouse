582ad1b0eab6e59f030f8ea6343e878687572c2f
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-08-26 15:06:42 +0700

    добавлен сериализатор пользовательских настроек

--- backend/api/lighthouse/serializers/serializer_setup.py
+++ backend/api/lighthouse/serializers/serializer_setup.py
@@ -1,6 +1,6 @@
 from lighthouse.appmodels.appsetup import AppSetup
 from rest_framework import serializers
-
+from .serializer_user import GroupListSerializer
 
 class AppSetupStringSerializer(serializers.ModelSerializer):
     code = serializers.CharField()
@@ -44,3 +44,24 @@ class AppSetupAllSerializer(serializers.ModelSerializer):
     class Meta:
         model = AppSetup
         fields = ['code', 'name', 'kind', 'intValue', 'strValue', 'floValue', 'dateValue']
+
+
+class UserSettingSerializer(serializers.Serializer):
+    login = serializers.CharField(source='user.username')
+    firstName = serializers.CharField(source='user.first_name')
+    lastName = serializers.CharField(source='user.last_name')
+    phone = serializers.CharField()
+    mail = serializers.CharField(source='user.email')
+    ntfPassword = serializers.BooleanField(source='ntf_password')
+    ntfCtrContract = serializers.BooleanField(source='ntf_ctl_contract')
+    ntfClaim = serializers.BooleanField(source='ntf_claim')
+    ntfPayment = serializers.BooleanField(source='ntf_payment')
+    groups = GroupListSerializer(source='user.groups', many=True, required=False)
+
+    def update(self, instance, validated_data):
+        instance.phone = validated_data['phone']
+        instance.ntf_password = validated_data['ntf_password']
+        instance.ntf_claim = validated_data['ntf_claim']
+        instance.ntf_payment = validated_data['ntf_payment']
+        instance.save()
+        return instance
