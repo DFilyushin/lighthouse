6a3021df706039f8588be1001d4c76e5ad9360ee
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-08-30 20:48:17 +0700

    подключен компонент отрисовки меню уведомлений

--- frontend/src/layouts/Main/components/Topbar/Topbar.tsx
+++ frontend/src/layouts/Main/components/Topbar/Topbar.tsx
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import clsx from 'clsx'
-import { makeStyles } from '@material-ui/core/styles'
-import { AppBar, Toolbar, Badge, Hidden, IconButton, Box, Typography, colors, Avatar } from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+import {AppBar, Toolbar, Badge, Hidden, IconButton, Box, Typography, colors, Avatar} from '@material-ui/core'
 import MenuIcon from '@material-ui/icons/Menu'
 import NotificationsIcon from '@material-ui/icons/NotificationsOutlined'
 import InputIcon from '@material-ui/icons/Input'
@@ -10,101 +10,124 @@ import {useDispatch, useSelector} from "react-redux";
 import {IStateInterface} from "../../../../redux/rootReducer";
 import {loadNotification} from "../../../../redux/actions/notificationAction";
 import {MINUTES_30_TIMES} from "../../../../utils/AppConst";
+import {NotificationsPopover} from "../../../../components";
+
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    boxShadow: 'none'
-  },
-  flexGrow: {
-    flexGrow: 1
-  },
-  title: {
-    flexGrow: 1,
-    color: theme.palette.getContrastText(colors.common.black)
-  },
-  signOutButton: {
-    marginLeft: theme.spacing(1)
-  },
-  large: {
-    width: theme.spacing(5),
-    height: theme.spacing(5),
-  },
-  orange: {
-    color: theme.palette.getContrastText(colors.common.white),
-    backgroundColor: colors.common.white,
-  },
+    root: {
+        boxShadow: 'none'
+    },
+    flexGrow: {
+        flexGrow: 1
+    },
+    title: {
+        flexGrow: 1,
+        color: theme.palette.getContrastText(colors.common.black)
+    },
+    signOutButton: {
+        marginLeft: theme.spacing(1)
+    },
+    large: {
+        width: theme.spacing(5),
+        height: theme.spacing(5),
+    },
+    orange: {
+        color: theme.palette.getContrastText(colors.common.white),
+        backgroundColor: colors.common.white,
+    },
 }));
 
-interface ITopbar {
-  className?: string,
-  onSidebarOpen?: any,
-  onSignout?: any
+interface ITopbarProps {
+    className?: string,
+    onSidebarOpen?: any,
+    onSignout?: any
 }
 
-const Topbar = (props: ITopbar) => {
-  const { className, onSidebarOpen, onSignout, ...rest } = props;
+const Topbar = (props: ITopbarProps) => {
+    const {className, onSidebarOpen, onSignout, ...rest} = props;
 
-  const classes = useStyles()
-  const dispatch = useDispatch()
+    const classes = useStyles()
+    const dispatch = useDispatch()
+    const notificationsRef = useRef(null)
+    const [openNotifications, setOpenNotifications] = useState(false);
 
-  const notices = useSelector((state: IStateInterface)=> state.notification.notices)
+    const notices = useSelector((state: IStateInterface) => state.notification.notices)
 
-  const getNotification = () => {
-    dispatch(loadNotification())
-  }
+    const getNotification = () => {
+        dispatch(loadNotification())
+    }
 
-  useEffect( ()=>{
-    getNotification();
-    setInterval(getNotification, MINUTES_30_TIMES);
-  }, [] )
+    const handleNotificationsClose = () => {
+        setOpenNotifications(false);
+    }
 
-  return (
-    <AppBar
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
-      <Toolbar>
-        <Hidden lgUp>
-          <IconButton
-              color="inherit"
-              onClick={onSidebarOpen}
-          >
-            <MenuIcon />
-          </IconButton>
-        </Hidden>
-        <Avatar className={clsx(classes.large, classes.orange)} variant='circle' >
-          <img src={logo} alt="Logo" height={32} width={32} />
-          </Avatar>
-        <Box display={{ xs: 'none', md: 'block', lg: 'block', xl: 'block' }}>
-          <Typography variant="h4" className={classes.title} >
-            &nbsp; Lighthouse - Управление производством
-          </Typography>
-        </Box>
-        <Box display={{ xs: 'block', md: 'none', lg: 'none', xl: 'none' }}>
-          <Typography variant="h6" className={classes.title} >
-            &nbsp; Lighthouse
-          </Typography>
-        </Box>
-        <div className={classes.flexGrow} />
+    const handleNotificationsOpen = () => {
+        setOpenNotifications(true);
+    }
 
-          <IconButton color="inherit">
-            <Badge
-              badgeContent={notices.length}
-              color="secondary"
-            >
-              <NotificationsIcon />
-            </Badge>
-          </IconButton>
-          <IconButton
-            className={classes.signOutButton}
-            color="inherit"
-            onClick={onSignout}
-          >
-            <InputIcon />
-          </IconButton>
-      </Toolbar>
-    </AppBar>
-  );
+    useEffect(() => {
+        getNotification();
+        setInterval(getNotification, MINUTES_30_TIMES);
+        // eslint-disable-next-line
+    }, [])
+
+    return (
+        <AppBar
+            {...rest}
+            className={clsx(classes.root, className)}
+        >
+            <Toolbar>
+                <Hidden lgUp>
+                    <IconButton
+                        color="inherit"
+                        onClick={onSidebarOpen}
+                    >
+                        <MenuIcon/>
+                    </IconButton>
+                </Hidden>
+                <Avatar className={clsx(classes.large, classes.orange)} variant='circle'>
+                    <img src={logo} alt="Logo" height={32} width={32}/>
+                </Avatar>
+                <Box display={{xs: 'none', md: 'block', lg: 'block', xl: 'block'}}>
+                    <Typography variant="h4" className={classes.title}>
+                        &nbsp; Lighthouse - Управление производством
+                    </Typography>
+                </Box>
+                <Box display={{xs: 'block', md: 'none', lg: 'none', xl: 'none'}}>
+                    <Typography variant="h6" className={classes.title}>
+                        &nbsp; Lighthouse
+                    </Typography>
+                </Box>
+                <div className={classes.flexGrow}/>
+
+                <IconButton
+                    color="inherit"
+                    onClick={handleNotificationsOpen}
+                    ref={notificationsRef}
+                >
+                    <Badge
+                        badgeContent={notices.length}
+                        color="secondary"
+                    >
+                        <NotificationsIcon/>
+                    </Badge>
+                </IconButton>
+                <IconButton
+                    className={classes.signOutButton}
+                    color="inherit"
+                    onClick={onSignout}
+                >
+                    <InputIcon/>
+                </IconButton>
+            </Toolbar>
+            <NotificationsPopover
+                anchorEl={notificationsRef.current}
+                notifications={notices}
+                onClose={handleNotificationsClose}
+                open={openNotifications}
+            />
+        </AppBar>
+    );
 };
 
 export default Topbar;
