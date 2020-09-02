893d58af8ac92c0fcc4b6a44902343a92b18f3b2
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-09-02 08:30:54 +0700

    компоненты перенесены в группу Клиент

--- frontend/src/views/Client/components/index.tsx
+++ frontend/src/views/Client/components/index.tsx
@@ -1,2 +1,3 @@
 export { default as ClientTable } from './ClientTable';
 export { default as ClientToolbar } from './ClientToolbar';
+export { default as ClientContractItem } from './ClientContractItem'
