991ee4b9da7c9528449d896ea55c977bc6ac7578
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-09-02 07:43:29 +0700

    добавлен необязательный параметр класса

--- frontend/src/views/Contract/components/ContractSkeletonLoading/ContractSkeletonLoading.tsx
+++ frontend/src/views/Contract/components/ContractSkeletonLoading/ContractSkeletonLoading.tsx
@@ -2,20 +2,21 @@ import React, {Fragment} from "react";
 import {Skeleton} from "@material-ui/lab";
 
 interface IContractSkeletonLoading {
+    className?: string;
     animation?: 'pulse' | 'wave' | false;
     height: number;
 }
 
 const ContractSkeletonLoading = (props: IContractSkeletonLoading) => {
-    const {animation, height} = props
+    const {animation, height, className} = props
 
 
     return (
-        <Fragment>
+        <div className={className}>
             <Skeleton animation={animation} height={height} width="100%" style={{marginBottom: 2}}/>
             <Skeleton animation={animation} height={height} width="100%" style={{marginBottom: 2}}/>
             <Skeleton animation={animation} height={height} width="100%" style={{marginBottom: 2}}/>
-        </Fragment>
+        </div>
     )
 }
 
