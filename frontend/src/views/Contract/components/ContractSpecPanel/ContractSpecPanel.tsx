f19191a471e126429670d052ed08cc615b22194e
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-09-08 07:30:44 +0700

    добавлен компонент панели для отображения списка спецификаций

---
+++ frontend/src/views/Contract/components/ContractSpecPanel/ContractSpecPanel.tsx
@@ -0,0 +1,96 @@
+import React, {useState} from 'react';
+import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
+import List from '@material-ui/core/List';
+import ListItem  from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Divider from '@material-ui/core/Divider';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ReceiptIcon from '@material-ui/icons/Receipt';
+import {IContractSpecItem} from "../../../../types/model/contract";
+import {RoundValue} from "../../../../utils/AppUtils";
+
+interface IContractSpecPanel {
+    items: IContractSpecItem[];
+    onClickItem: (numItem: string) => void;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            width: '100%',
+            maxWidth: 360,
+            backgroundColor: theme.palette.background.paper,
+        },
+    }),
+);
+
+
+const ContractSpecPanel = (props: IContractSpecPanel) => {
+    const {items, onClickItem} = props
+    const classes = useStyles();
+
+    const [selectedIndex, setSelectedIndex] = useState('1')
+
+    /**
+     * Получить сортированный уникальный список номеров спецификации
+     */
+    const getSpecNums = () => {
+        const newList: any [] = []
+        items.forEach((item) => {
+            if (!newList.find(value => value.num === item.specNum)) {
+                const newItem = {num: item.specNum, date: item.specDate}
+                newList.push(newItem)
+            }
+        })
+        return newList.sort(function (a, b) {
+            const nameA=a.num.toLowerCase()
+            const nameB=b.num.toLowerCase()
+            if (nameA < nameB) //сортируем строки по возрастанию
+                return -1
+            if (nameA > nameB)
+                return 1
+            return 0 // Никакой сортировки
+        })
+    }
+
+    /**
+     * Общая сумма спецификации
+     */
+    const getSpecTotalSum = () => {
+        return RoundValue(items.map(({itemTotal}) => itemTotal).reduce((sum, i) => sum + i, 0))
+    }
+
+    return (
+        <div className={classes.root}>
+            <List component="nav" aria-label="secondary mailbox folders">
+                {
+                    getSpecNums().map((item)=>
+                        (
+                        <ListItem button
+                                  key={item.num}
+                                  selected={selectedIndex === item.num}
+                                  onClick={()=> {
+                                      onClickItem(item.num)
+                                      setSelectedIndex(item.num)
+                                  }}
+                        >
+                            <ListItemText primary={`#${item.num} от ${item.date}`} secondary="256000 т."/>
+                        </ListItem>
+                        )
+                    )
+                }
+            </List>
+            <Divider />
+            <List component="nav" aria-label="secondary mailbox folders">
+                <ListItem>
+                    <ListItemIcon>
+                        <ReceiptIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={`${getSpecTotalSum()} тенге`} />
+                </ListItem>
+            </List>
+        </div>
+    )
+}
+
+export default ContractSpecPanel
