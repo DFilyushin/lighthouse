bd23bf15055621125975e1dc336778307ca04fa1
Dmitriy Filyushin <dmitriy.filyushin@yandex.ru>
2020-09-24 22:31:02 +0700

    при отсутствии статуса не включается фильтр

--- backend/api/lighthouse/endpoints/api_prod.py
+++ backend/api/lighthouse/endpoints/api_prod.py
@@ -49,7 +49,8 @@ class ProductionView(viewsets.ModelViewSet):
             if product is not None:
                 queryset = queryset.filter(id_formula__id_product=product)
             if state is not None:
-                queryset = queryset.filter(cur_state=int(state))
+                if int(state) != -1:
+                    queryset = queryset.filter(cur_state=int(state))
             if start_period and not card_num and (state is None or int(state) > CARD_STATE_IN_WORK):
                 date_start = datetime.strptime(start_period, '%Y-%m-%d')
                 date_end = datetime.strptime(end_period, '%Y-%m-%d')
