ea28c34c017f549bc1a74a7ae6c3bb14b511903c
DFilyushin <dmitriy.filyushin@yandex.ru>
2020-10-13 17:22:10 +0700

    добавлен импорт файла

--- backend/api/lighthouse/models.py
+++ backend/api/lighthouse/models.py
@@ -3,6 +3,7 @@ from .appmodels.org import *
 from .appmodels.sales import *
 from .appmodels.db_signals import *
 from .appmodels.appsetup import *
+from .appmodels.store import *
 
 # =================================
 # Файл не удалять
