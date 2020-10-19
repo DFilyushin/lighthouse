ba1d14fddc3a278613be652571a4c69a8a0910e2
Dmitriy Filyushin <dmitriy.filyushin@yandex.ru>
2020-10-19 18:18:14 +0700

    добавлена функция загрузки материалов loadMaterials в состояние "Материалы"

--- frontend/src/redux/actions/materialAction.ts
+++ frontend/src/redux/actions/materialAction.ts
@@ -1,5 +1,8 @@
 import authAxios from "../../services/axios-api";
 import MaterialEndpoint from "../../services/endpoints/MaterialEndpoint";
+import {hideInfoMessage, showInfoMessage} from "./infoAction";
+import {MATERIAL_LOAD_FINISH, MATERIAL_LOAD_START, MATERIAL_LOAD_SUCCESS} from "./types";
+import {IMaterial} from "../../types/model/material";
 
 /**
  * Получить название материала
@@ -8,4 +11,46 @@ import MaterialEndpoint from "../../services/endpoints/MaterialEndpoint";
 export async function getMaterialName(materialId: number) {
     const response = await authAxios.get(MaterialEndpoint.getMaterialItem(materialId))
     return Promise.resolve(response.data['name'])
+}
+
+/**
+ * Получить список материалов
+ */
+export function loadMaterials() {
+    return async (dispatch: any, getState: any) => {
+        dispatch(fetchStart());
+        dispatch(hideInfoMessage())
+        try {
+            const url = MaterialEndpoint.getMaterialList()
+            const materialList: IMaterial[] = [];
+            const response = await authAxios.get(url);
+            Object.keys(response.data).forEach((key, index) => {
+                materialList.push(response.data[key])
+            });
+            dispatch(fetchSuccess(materialList))
+        } catch (e) {
+            dispatch(showInfoMessage('error', e.toString()))
+        }
+        dispatch(fetchFinish())
+    }
+}
+
+
+function fetchStart() {
+    return {
+        type: MATERIAL_LOAD_START
+    }
+}
+
+function fetchFinish() {
+    return {
+        type: MATERIAL_LOAD_FINISH
+    }
+}
+
+function fetchSuccess(items: IMaterial[]) {
+    return {
+        type: MATERIAL_LOAD_SUCCESS,
+        items
+    }
 }
